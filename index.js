18909b9f56a21d2e1e3348420a2c3b4561e5795f
Tijuana22 <tijuanahunt39@gmail.com>
2022-12-07 12:36:31 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function minusOne(num10){
+    return num10 - 1;
+}
+console.log(minusOne(10) -1);
+
+
+function makeSentence(){
+    const x = 'I';
+    const y = 'want';
+    const z = 'Chimichangas';
+
+    return 'Oh boy, do  '+ x +' '+ y +' '+ z +' or what?';
+}
+console.log(makeSentence());
+
+
+function getLastElement(arr){
+    return arr[arr.length-1]
+}
+
+    
+
+console.log(getLastElement([1,2,3,4,5]));
